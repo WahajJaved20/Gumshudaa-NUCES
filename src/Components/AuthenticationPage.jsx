e065adb976769b7d247bfa70c8efebbdad63c16b
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-12-14 18:02:08 +0500

    Transitions UP

--- src/Components/AuthenticationPage.jsx
+++ src/Components/AuthenticationPage.jsx
@@ -3,9 +3,9 @@ import { frontLogo } from '../assets';
 
 const AuthenticationPage = () => {
   const [isLogin, setLogin] = React.useState(true);
-  const visibility = {
-    true : "border-4",
-    false : ""
+  const handleTransition = (state) => {
+    setLogin(state)
+    console.log(isLogin)
   }
   return (
     <div className="flex flex-col md:flex-row lg:flex-row h-screen">
@@ -16,24 +16,18 @@ const AuthenticationPage = () => {
       <div className="flex-1 bg-primaryColor flex flex-col items-center justify-center p-8">
       <div className="bg-tertiaryColor flex flex-col md:flex-row rounded-3xl w-full max-w-lg border-4 border-solid border-black p-2 mb-4">
       <button
-        className={`flex-1 mb-2 text-xl font-Changa font-bold border-black  rounded-full mb-2 md:mb-0 md:mr-2 ${
-          isLogin ? 'bg-white border-4' : 'bg-tertiaryColor'
-        }`}
-      >
-        LOGIN
+        className={`flex-1 mb-2 text-xl font-Changa font-bold border-black  rounded-full 
+        mb-2 md:mb-0 md:mr-2 ${isLogin ? 'bg-white border-4' : 'bg-tertiaryColor'}` }
+        onClick={()=>{handleTransition(true)}}>
+          LOGIN
       </button>
       <button
-        className={`flex-1 text-xl font-Changa font-bold border-black rounded-full md:ml-2 ${
-          !isLogin ? 'bg-white border-4 ' : 'bg-tertiaryColor'
-        }`}
-      >
+        className={`flex-1 text-xl font-Changa font-bold border-black 
+        rounded-full md:ml-2 ${!isLogin ? 'bg-white border-4 ' : 'bg-tertiaryColor'}`}
+        onClick={()=>{handleTransition(false)}}>
         REGISTER
       </button>
     </div>
-      {/* <div className="bg-white flex flex-row rounded-3xl w-full max-w-lg border-4 border-solid border-black h-[10%] p-2 mb-4">
-        <button className={`${visibility[isLogin]} flex-1  text-xl  font-Changa font-bold border-black rounded-full`}>LOGIN</button>
-        <button className={`${visibility[!isLogin]} flex-1 text-xl font-Changa font-bold border-black rounded-full`} >REGISTER</button>
-      </div> */}
         <div className="hidden md:block lg:block bg-white rounded-3xl w-full max-w-lg border-4 border-solid border-black h-[70%] p-8">
         <div className='mt-8'/>
           <label className="text-2xl md:text-3xl lg:text-3xl font-bold font-Changa mb-4">NU EMAIL</label>
@@ -43,7 +37,7 @@ const AuthenticationPage = () => {
           <input className="border-b-4 font-bold text-xl border-black focus:outline-none font-Changa w-full mb-4 mt-4" type="password" />
           <div className='mt-8'/>
           <button className="bg-white text-2xl font-Changa text-black border-4 border-black rounded-full h-[12%] mt-4 font-bold ml-[70%] w-[30%]">
-            LOGIN
+          {isLogin ? "LOGIN" : "REGISTER"}
           </button>
         </div>
 
@@ -53,8 +47,8 @@ const AuthenticationPage = () => {
           <label className="text-2xl font-bold font-Changa mb-2 mt-4">PASSWORD</label>
           <input className="border-b-4 font-bold text-xl border-black focus:outline-none font-Changa w-full mb-4 mt-4" type="password" />
 
-          <button className="bg-white text-xl font-Changa text-black border-4 border-black rounded-full h-[12%] mt-4 ml-[70%] font-bold w-[30%]">
-            LOGIN
+          <button className="bg-white text-xl font-Changa text-black border-4 border-black rounded-full h-[14%] mt-4 ml-[60%] font-bold w-[50%]">
+            {isLogin ? "LOGIN" : "REGISTER"}
           </button>
         </div>
       </div>
