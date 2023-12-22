100d69bbf1a623e4ce429f9318b8347f19704215
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-12-22 20:36:30 +0500

    dynamic button

--- src/Components/ContentCard.jsx
+++ src/Components/ContentCard.jsx
@@ -1,9 +1,26 @@
 import React, { useEffect } from "react";
+import { toast } from "react-toastify";
 
-const ContentCard = ({ data }) => {
+const ContentCard = ({ data, user }) => {
   useEffect(() => {
-    console.log(data);
-  })
+    console.log(user);
+  }, [data]);
+  const handleClick = () => {
+    try{
+      const response = fetch("https://server-gumshuda-nuces.vercel.app/markAsDone", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: data._id }),
+      });
+      console.log(response);
+      toast.success("Marked as Closed");
+    }catch(err){
+      console.log(err);
+      toast.error("Error Occured");
+    }
+  }
   return (
     <div className="flex bg-white rounded-lg shadow-md p-6 mb-4">
        <div className="flex-shrink-0 flex-col">
@@ -19,8 +36,8 @@ const ContentCard = ({ data }) => {
          <p className="mb-2 font-Changa font-bold truncate">{data.userId}</p>
          <p className="font-Changa font-bold truncate">{data.locationFound}</p>
          <p className="font-Changa font-bold truncate">{data.date}</p>
-         <button className={`${!data.closed ? "bg-green-500 " : "bg-red-500 "}font-Changa font-bold truncate text-white font-bold py-2 px-4 rounded ${!data.closed ? " hover:bg-red-500" : " hover:bg-green-500"}`}>
-           {data.closed ? "Open" : "Closed"}
+         <button onClick={handleClick} disabled={(user !== data.userId) || data.closed} className={`${!data.closed ? "bg-green-500 " : "bg-red-500 "}font-Changa font-bold truncate text-white font-bold py-2 px-4 rounded ${!data.closed ? " hover:bg-red-500" : " hover:bg-green-500"}`}>
+           {!data.closed ? user === data.userId ? "Mark as Closed" : "Open" : "Closed"}
          </button> 
        </div>
 
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -14,7 +14,6 @@ const HomePage = () => {
         const fetchData = async (user) => {
             const response = await fetch("https://server-gumshuda-nuces.vercel.app/get");
             let data = await response.json();
-            data = data.sort((a, b) => a.closed > b.closed);
             data = data.sort((a, b) => {
                 const aMatches = a["userId"] === user;
                 const bMatches = b["userId"] === user;
@@ -26,6 +25,17 @@ const HomePage = () => {
                   return 0;
                 }
               });
+              data = data.sort((a, b) => {
+                const aMatches = a["closed"] === false;
+                const bMatches = b["closed"] === false;
+                if (aMatches && !bMatches) {
+                  return -1;
+                } else if (!aMatches && bMatches) {
+                  return 1; 
+                } else {
+                  return 0;
+                }
+            });
             console.log(data);
             setCardData(data);
             console.log("Calling Server")
@@ -65,7 +75,7 @@ const HomePage = () => {
                 <>
                     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
                     {cardsData.map((data, index) => (
-                        <ContentCard key={index} data={data} />
+                        <ContentCard key={index} data={data} user={currentUser}/>
                     ))}
                     </div>
                 </>
