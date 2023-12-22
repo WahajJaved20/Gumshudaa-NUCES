aa17f32c4c52faad4d754c2e4a803e033617f925
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-12-22 18:22:47 +0500

    redirect page

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Components/HomePage.jsx'
 import PostItem from './Components/PostItem.jsx'
 import { ToastContainer } from 'react-toastify';
+import NotFoundPage from './Components/RedirectError.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/post" element={<PostItem />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
     <ToastContainer
